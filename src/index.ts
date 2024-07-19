0551e41a10c60865c98e274c06a0bc349498d986
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-07-18 23:05:51 -0300

    router controller

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import colors from "colors"
 import { app } from "./server"
-import { ProjectRouter } from "./routes/projectRouter"
 
 
 const port = process.env.PORT || 4000
 
-app.use("/api/projects", ProjectRouter)
 
 app.listen(port, () => {
     console.log(colors.america(`API en el Puerto: ${port}`))
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from "express"
-import dotenv from "dotenv"
 import { connectDB } from "./config/db"
+import { ProjectRouter } from "./routes/projectRouter"
+
+import dotenv from "dotenv"
 dotenv.config()
-
-
 connectDB()
+
+
 export const app = express()
+
+app.use("/api/projects", ProjectRouter)
