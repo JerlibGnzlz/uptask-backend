9fa1e8420108721371445c2fc1940e171e97f597
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-07-20 22:10:50 -0300

    task

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,10 +1,23 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
 
 
+const taskStatus = {
+    PENDING: "pending",
+    ON_HOLD: "onHold",
+    IN_PROGRESS: "inProgress",
+    UNDER_REVIEW: "underReview",
+    COMPLETE: "complete"
+} as const
+
+
+export type TaskStatus = typeof taskStatus[keyof typeof taskStatus]
+
+
 export interface ITask extends Document {
     name: string
     description: string,
-    project: Types.ObjectId
+    project: Types.ObjectId,
+    status: TaskStatus
 }
 
 
@@ -23,6 +36,11 @@ const taskSchema: Schema = new Schema({
     project: {
         type: Types.ObjectId,
         ref: "Project"
+    },
+    status: {
+        type: String,
+        enum: Object.values(taskStatus),
+        default: taskStatus.PENDING
     }
 }, {
     timestamps: false,
