2423dda72055c8a629c8143b4098a4f1198c301b
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-07-31 18:13:34 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import colors from "colors"
 import { app } from "./server"
 
-
 const port = process.env.PORT || 4000
-
 
 app.listen(port, () => {
     console.log(colors.bgBlue.black.bold(`API en el Puerto: ${port}`))
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
-import express, { json } from "express"
+import express from "express"
 import { router as ProjectRouter } from "./routes/projectRouter"
 import { connectDB } from "./config/db"
-
 import dotenv from "dotenv"
+import cors from 'cors';
+import { corsConfig } from "./config/cors";
 dotenv.config()
-
 
 connectDB()
 export const app = express()
+
+app.use(cors(corsConfig))
+
 app.use(express.json())
 
 app.use("/api/projects", ProjectRouter)
