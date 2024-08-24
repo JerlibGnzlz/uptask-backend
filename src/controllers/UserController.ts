f4557161d9b4831df4696fd565677a4044bb1007
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-08-24 16:31:18 -0300

    userExist en db

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,26 @@
 import type { Request, Response } from "express"
 import { User } from '../models/User';
+import { hashPassword } from "../utils/auth";
 
 
 export class UserController {
     static createUser = async (req: Request, res: Response) => {
 
-        const user = new User(req.body)
         try {
+            const { password, email } = req.body
+            /* --------------------------- Prevenir duplicados -------------------------- */
+            const userExist = await User.findOne({ email })
+            if (userExist) {
+                const error = new Error("El Usuario ya esta registrado")
+                return res.status(409).json({ error: error.message })
+            }
+
+            /* ----------------------------- Crea un Usuario ---------------------------- */
+            const user = new User(req.body)
+
+            /* ------------------------------ Hash password ----------------------------- */
+
+            user.password = await hashPassword(password)
             await user.save()
             res.json("Cuenta creada, revisa tu email para confirmarla")
         } catch (error) {
