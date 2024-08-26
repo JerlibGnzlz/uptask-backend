505f838dcf1c12b8e91677fdfcafa1d01145eeca
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-08-26 17:19:06 -0300

    model token

---
+++ src/config/nodemailer.ts
@@ -0,0 +1,16 @@
+import nodemailer from "nodemailer"
+
+
+const config = () => {
+    return {
+        host: process.env.NODEMAILER_HOST,
+        port: process.env.NODEMAILER_PORT,
+        auth: {
+            user: process.env.NODEMAILER_USER,
+            pass: process.env.NODEMAILER_PASS
+        }
+    }
+
+}
+
+export const transport = nodemailer.createTransport(config());
